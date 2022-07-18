04eacd2b64c4765d465b37c587b35da993c6fe04
Youssef Lahssini <58909647+ylahssini@users.noreply.github.com>
2022-07-18 21:14:11 +0100

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -5,11 +5,9 @@ rocketSelector.addEventListener("mousemove", function (event) {
   const x = event.clientX - window.innerWidth / 2;
   const y = event.clientY - window.innerHeight / 2;
 
-  rocketImageSelector.style.transition = "none";
   rocketImageSelector.style.transform = `translate3d(${-x / 10}px, ${-y / 10}px, 0)`;
 });
 
 rocketSelector.addEventListener("mouseleave", function () {
-  rocketImageSelector.style.transition = "transform .3s ease-in";
   rocketImageSelector.style.transform = "translate3d(0, 0, 0)";
 });
